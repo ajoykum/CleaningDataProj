01ece57498c5ccdf84bf18fc14028d283f5c0b99
Ajoy Kumar Palanivelu <ajoykum@gmail.com>
2017-06-23 22:50:08 -0400

    codebook addition

---
+++ codebook.md
@@ -0,0 +1,122 @@
+# CleaningDataProj Code Book
+
+## Objective
+The goal is to prepare tidy data set from a raw dataset downloaded from Internet
+
+## Input Data set
+The URL presented below houses the data obtained from a research project on wearable computing
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## Input data summary
+
+* Data downloaded from the site is present in the folder Rawdata
+* 70% of observations are observed in Training Data Set folder
+* 30% of observations are observed in Test Data Set folder
+* 6 different activities were tracked during the experimental period
+* 30 different subject/participant's data is provided
+* Main data set has more than 561 feature vectors captured
+
+## Data Preparatory Steps
+
+* Downloaded the file from URL
+* Unzipped the file
+* Using read.fwf function read the test data set
+* Column appended the subjects for the test data set
+* Column appended the activity code for the test data set
+* Repeated the 3 above steps for train data set
+* Merged the two dataset using Rbind to arrive at combined data set
+* Using grep command reduced the data frame to have only mean and std columns 
+* Read the activity label file to get activity code description
+* Merged the combined data set with activity code file to get activity description
+* Create a new data set using aggregate function, classified by Activity and Subject and apply Mean function to get Subject level, activity level mean values of different measures
+* Parse the column names to make it readable using substitute text operations
+* write the output into a .csv file using write.table command
+
+
+
+### Output Data Set
+Output of the script is stored in a .csv file named as 'OutputDataset.csv'
+
+The following variables are documented in the Outputdataset
+
+## activitydescription	
+  Categorical variable with 6 different values
+  *VALUES: WALKING, WALKING_DOWNSTAIRS, WALKING_UPSTAIRS, SITTING, STANDING,  LAYING*
+
+## subjectcode	
+  Factor variable identifiying 30 different values for different participants
+  *vALUES: 1 TO 30*
+  
+## TIME SERIES VARIABLES
+*vALUES RANGE FROM -1 TO 1*
+Time Series Body Acceleration-mean()-X	
+Time Series Body Acceleration-mean()-Y	
+Time Series Body Acceleration-mean()-Z	
+Time Series Body Acceleration-std()-X	
+Time Series Body Acceleration-std()-Y	
+Time Series Body Acceleration-std()-Z	
+Time Series GravityAcceleration-mean()-X	
+Time Series GravityAcceleration-mean()-Y	
+Time Series GravityAcceleration-mean()-Z	
+Time Series GravityAcceleration-std()-X	
+Time Series GravityAcceleration-std()-Y	
+Time Series GravityAcceleration-std()-Z	
+Time Series Body AccelerationJerk-mean()-X	
+Time Series Body AccelerationJerk-mean()-Y	
+Time Series Body AccelerationJerk-mean()-Z	
+Time Series Body AccelerationJerk-std()-X	
+Time Series Body AccelerationJerk-std()-Y	
+Time Series Body AccelerationJerk-std()-Z	
+Time Series Body Gyro-mean()-X	
+Time Series Body Gyro-mean()-Y	
+Time Series Body Gyro-mean()-Z	
+Time Series Body Gyro-std()-X	
+Time Series Body Gyro-std()-Y	
+Time Series Body Gyro-std()-Z	
+Time Series Body GyroJerk-mean()-X	
+Time Series Body GyroJerk-mean()-Y	
+Time Series Body GyroJerk-mean()-Z	
+Time Series Body GyroJerk-std()-X	
+Time Series Body GyroJerk-std()-Y	
+Time Series Body GyroJerk-std()-Z	
+Time Series Body Acceleration Magnitude-mean()	
+Time Series Body Acceleration Magnitude-std()	
+Time Series GravityAcceleration Magnitude-mean()	
+Time Series GravityAcceleration Magnitude-std()	
+Time Series Body AccelerationJerk Magnitude-mean()	
+Time Series Body AccelerationJerk Magnitude-std()	
+Time Series Body Gyro Magnitude-mean()	
+Time Series Body Gyro Magnitude-std()	
+Time Series Body GyroJerk Magnitude-mean()	
+Time Series Body GyroJerk Magnitude-std()	
+
+## FREQUENCY SERIES VARIABLES
+*vALUES RANGE FROM -1 TO 1*
+Frequency Series Body Acceleration-mean()-X	
+Frequency Series Body Acceleration-mean()-Y	
+Frequency Series Body Acceleration-mean()-Z	
+Frequency Series Body Acceleration-std()-X	
+Frequency Series Body Acceleration-std()-Y	
+Frequency Series Body Acceleration-std()-Z	
+Frequency Series Body AccelerationJerk-mean()-X	
+Frequency Series Body AccelerationJerk-mean()-Y
+Frequency Series Body AccelerationJerk-mean()-Z
+Frequency Series Body AccelerationJerk-std()-X
+Frequency Series Body AccelerationJerk-std()-Y
+Frequency Series Body AccelerationJerk-std()-Z
+Frequency Series Body Gyro-mean()-X	
+Frequency Series Body Gyro-mean()-Y	
+Frequency Series Body Gyro-mean()-Z	
+Frequency Series Body Gyro-std()-X	
+Frequency Series Body Gyro-std()-Y	
+Frequency Series Body Gyro-std()-Z	
+Frequency Series Body Acceleration Magnitude-mean()	
+Frequency Series Body Acceleration Magnitude-std()	
+Frequency Series Body AccelerationJerk Magnitude-mean()
+Frequency Series Body AccelerationJerk Magnitude-std()
+Frequency Series Body Gyro Magnitude-mean()
+Frequency Series Body Gyro Magnitude-std()
+Frequency Series Body GyroJerk Magnitude-mean()
+Frequency Series Body GyroJerk Magnitude-std()
+
---
+++ run_analysis.R
@@ -0,0 +1,156 @@
+library(stats)
+library(dplyr)
+
+## Check if pro director exist
+## Create the directory if it does not already exist
+## Set the working directory to the newly created dir
+## create conn variable to store the url
+## download the file and store it locally as UCI.zip
+
+
+
+
+if(!file.exists("./pro")){dir.create("./pro")}
+
+setwd("./pro")
+
+con <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+print("Attempting to download a 59MB file..")
+if(!file.exists("uci.zip"))
+{
+  download.file(con,"uci.zip",mode = "wb",cacheOK = TRUE)
+}
+
+## check and print if the file was downloaded. 
+
+if (file.exists("uci.zip")){ print("file is available") }
+
+## Unzip the file 
+
+print("unzipping the downloaded file")
+
+unzip(zipfile = "uci.zip",overwrite = TRUE)
+
+## set the working directory to the training folder 
+
+setwd("./UCI HAR Dataset/test")
+
+## set a variable as a widths delimiter to read the fixed width txt file as a dataset
+
+wid<-rep(16,561)
+
+## create a data frame to read the text file which has test data set
+
+print("reading data file.. typically takes some time")
+
+teds <- read.fwf(file = "X_test.txt", widths = wid,header = FALSE)
+
+## create a dataframe to read the activity code for each of the observation
+
+tedsy <- read.fwf(file = "y_test.txt",widths = 2, header = FALSE)
+
+## create a data frame to read the text file which has subject's id
+tedsub <- read.fwf(file = "subject_test.txt", widths = 2, header = FALSE)
+
+
+## insert a column in test data frame to include activity code
+teds$actcode<-tedsy$V1
+
+## insert a column to identify the row with subject id / participant
+teds$participant<-tedsub$V1
+
+## Move to parent directory
+setwd('..')
+
+getwd()
+
+print("Another big file to read.. ")
+
+## Read the text file which has training data set
+trds <- read.fwf(file = "./train/X_train.txt", widths = wid,header = FALSE)
+
+## Read the text file which has training data set activity code
+trdsy <- read.fwf(file = "./train/y_train.txt",widths = 2, header = FALSE)
+
+## Read the text file which has training subject / participant code
+trdsub <- read.fwf(file = "./train/subject_train.txt",widths = 2, header = FALSE)
+
+## append training activity labels and participants with training data set
+trds$actcode <- trdsy$V1
+trds$particpant <- trdsub$V1
+
+
+
+## Features table has the name of all the columns. Ingest the column names of the 561 features available in features.txt
+
+feat<-read.table(file = 'features.txt',header = FALSE, sep = ' ',stringsAsFactors = FALSE)
+
+## assigning column names to train data set for the first 561 features
+colnames(trds)<-feat[[2]]
+
+## assigning column names for the activity code and subject code 
+colnames(trds)[562]<-as.character("activitycode")
+colnames(trds)[563]<-as.character("subjectcode")
+
+
+## assigning column names to test data set for the first 561 features
+colnames(teds)<-feat[[2]]
+
+## assigning column names for the activity code and subject code 
+colnames(teds)[562]<-as.character("activitycode")
+colnames(teds)[563]<-as.character("subjectcode")
+
+## vertically merge the two datasets resulting in a new data set
+
+comb <- rbind.data.frame(trds,teds)
+
+print("Merged the datafiles successfully")
+## Using grep find all the column names which has either a 'mean' or 'std' in the column names. Create a new dataset only with this 
+## Using [^F] to avoid pulling the meanFreq variables which are present in the dataset
+
+comb1 <- comb[,grep("mean[^F]|std",colnames(comb))]
+
+## add activity code and subject code to the shortened table
+
+comb1$activitycode<-as.factor(comb$activitycode)
+
+comb1$subjectcode<-as.factor(comb$subjectcode)
+
+print("Removed non-mean, non-std data columns")
+## reading activity description from activity_labels file
+
+actlabel<-read.table(file = "activity_labels.txt", header = FALSE, sep = " ",col.names = c("activitycode","activitydescription"))
+
+## merging activity lables with combined data set by id to get activity description
+comb1<-merge(x = comb1,y = actlabel,by.x = "activitycode",by.y = "activitycode",all = TRUE)
+
+## We have a data set which is merged, has activity description, has descriptive data values and has only Mean and SD variables
+print("We have added descriptity activity to dataset")
+
+##creating a independent tidy data set with the average of each variable for each activity and each subject.
+
+newd<-aggregate(x = comb1[2:67],by = comb1[c("activitydescription","subjectcode")], FUN = mean)
+
+print("Created a new tiday dataset")
+## creating descriptive names for columns
+## replacing Acc with Acceleration
+colnames(newd)<-gsub(pattern = "Acc",replacement = "Acceleration",x = colnames(newd))
+
+## replacing Mag with tbody and tgravity with Time series Body
+colnames(newd)<-gsub(pattern = "tBody",replacement = "Time Series Body",x = colnames(newd))
+
+colnames(newd)<-gsub(pattern = "tGravity",replacement = "Time Series Gravity",x = colnames(newd))
+
+colnames(newd)<-gsub(pattern = "fBody",replacement = "Frequency Series Body",x = colnames(newd))
+
+colnames(newd)<-gsub(pattern = "BodyBody",replacement = "Body ",x = colnames(newd))
+
+colnames(newd)<-gsub(pattern = "BodyGyro",replacement = "Body Gyro",x = colnames(newd))
+
+colnames(newd)<-gsub(pattern = "BodyAcceleration",replacement = "Body Acceleration",x = colnames(newd))
+
+# write the content of Output file in OutputDataset.csv
+write.csv(x = newd,file = "OutputDataset.csv")
+
+View(newd)
